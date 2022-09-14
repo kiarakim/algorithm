30a9ec6f03057d89389b3f01d3b26f016ad3dbec
Kiara Kim <101039161+kiarakim@users.noreply.github.com>
2022-09-14 22:25:30 +0900

    add 정렬.cpp files

--- Project/2750.cpp
+++ Project/2750.cpp
@@ -1,20 +1,27 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int num;
-    int arr[1000] = { 0, };
-    int tmp;
-    cin >> num;
-    for (int i = 0; i < num; i++)
-        cin >> arr[i];
-    for (int i = num; i > 1; i--)
-        for (int j = 0; j + 1 < i; j++)
-            if (arr[j] > arr[j + 1])
-            {
-                tmp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = tmp;
-            }
-    for (int i = 0; i < num; i++)
-        cout << arr[i] << '\n';
+
+	int n, temp;
+	int arr[1000];
+
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = i + 1; j < n; j++) {
+			if (arr[i] > arr[j]) {
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		if (i + 1 < n && arr[i] == arr[i + 1]) continue;
+		cout << arr[i] << "\n";
+	}
+
 }
