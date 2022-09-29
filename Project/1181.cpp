baa6462a6222e8ea47f5ccb40f76d86244511bcf
Kiara Kim <101039161+kiarakim@users.noreply.github.com>
2022-09-29 20:22:36 +0900

    add baekjoon .cpp files

--- Project/1181.cpp
+++ Project/1181.cpp
@@ -4,9 +4,28 @@
 #include<utility>
 using namespace std;
 
+int N;
+vector<pair<int, string>> v;
+
 int main() {
 
+	cin >> N;
 
+	for (int i = 0; i < N; i++) {
+		string str;
+		cin >> str;
+		int len = str.length();
+		v.push_back({ len, str });
+	}
+
+	sort(v.begin(), v.end());
+
+	cout << v[0].second << "\n";
+
+	for (int i = 1; i < N; i++) {
+		if (v[i].second == v[i - 1].second) continue;
+		cout << v[i].second << "\n";
+	}
 
 	return 0;
 }
