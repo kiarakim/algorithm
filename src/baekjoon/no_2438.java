79c894e9cabed1d36249dd269448cb75e27ad233
kiarakim <kiarakim413@gmail.com>
2022-10-25 09:47:04 +0900

    add .java files

--- src/baekjoon/no_2438.java
+++ src/baekjoon/no_2438.java
@@ -4,16 +4,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class no_9093 {
+public class no_2438 {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        int A = Integer.parseInt(br.readLine());
 
         StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < N; i++) {
-            sb.append(i);
+        for (int i = 1; i <= A; i++) {
+            for (int j = 1; j <= i; j++) {
+                sb.append('*');
+            }
+            sb.append('\n');
         }
         System.out.println(sb);
     }
