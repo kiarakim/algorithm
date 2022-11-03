86a52f9c56ca45c3189944e85e8796920f0aabc9
kiarakim <kiarakim413@gmail.com>
2022-11-03 15:46:37 +0900

    delete StringTockenizer library

--- src/baekjoon/no_2739.java
+++ src/baekjoon/no_2739.java
@@ -3,7 +3,6 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class no_2739 {
     public static void main(String[] args) throws IOException {
