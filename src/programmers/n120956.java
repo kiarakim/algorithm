b74d2e5bfbc97d66b81e9a649beff57239fa59fc
kiarakim <kiarakim413@gmail.com>
2022-11-10 12:44:20 +0900

    feat(120956) : add skeleton code

---
+++ src/programmers/n120956.java
@@ -0,0 +1,13 @@
+package programmers;
+
+public class n120956 {
+    public static void main(String[] args) {
+        String[] babbling = {"aya", "yee","u","maa","wyeoo"};
+        int result = solution(babbling);
+    }
+    public static int solution(String[] babbling){
+        int answer = 0;
+        return answer;
+    }
+
+}
