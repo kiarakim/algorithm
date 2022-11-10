d2fc4fa189605709454b47bcf404798a66a3f246
kiarakim <kiarakim413@gmail.com>
2022-11-10 13:06:12 +0900

    feat(120924) : add skeleton code

---
+++ src/programmers/n120924.java
@@ -0,0 +1,14 @@
+package programmers;
+
+public class n120924 {
+    public static void main(String[] args) {
+        int[] common = {1,2,3,4};
+        int result = solution(common);
+        System.out.println(result);
+    }
+
+    public static int solution(int[] common) {
+        int answer = 0;
+        return answer;
+    }
+}
