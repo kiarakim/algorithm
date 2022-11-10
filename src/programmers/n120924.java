026fe64191d9bc095c3cba4d582f1423fd23847d
kiarakim <kiarakim413@gmail.com>
2022-11-10 13:32:08 +0900

    feat(120924) : get difference and ratio, then compute

--- src/programmers/n120924.java
+++ src/programmers/n120924.java
@@ -9,6 +9,17 @@ public class n120924 {
 
     public static int solution(int[] common) {
         int answer = 0;
+        int commonLength = common.length;
+
+        if ((common[1] - common[0]) == (common[2] - common[1])) {
+            int difference = common[1] - common[0];
+            answer = common[commonLength - 1] + difference;
+        }
+
+        else if ((common[1] / common[0]) == (common[2] / common[1])) {
+            int ratio = common[1] / common[0];
+            answer = common[commonLength - 1] * ratio;
+        }
         return answer;
     }
 }
