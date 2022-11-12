52c6b30bc23f7a919f27741d22fcc44d4017dbd5
kiarakim <kiarakim413@gmail.com>
2022-11-12 23:26:36 +0900

    feat(n120826) : add skeleton code

---
+++ src/programmers/n120826.java
@@ -0,0 +1,15 @@
+package programmers;
+
+public class n120826 {
+    public static void main(String[] args) {
+        n120826 sol = new n120826();
+        String my_string = "BCBdbe";
+        char letter = 'B';
+        System.out.println(sol.solution(my_string, letter));
+    }
+
+    public String solution(String my_string, char letter) {
+        String answer = "";
+        return answer;
+    }
+}
