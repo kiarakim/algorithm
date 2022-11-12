22a7bdf604c8d622353782ff4be38ffc2e87ff16
kiarakim <kiarakim413@gmail.com>
2022-11-12 23:36:07 +0900

    feat(n120826) : Implement eliminating char from string
    In order to add a character in String during for loop, it's better to make String a StringBuilder

--- src/programmers/n120826.java
+++ src/programmers/n120826.java
@@ -4,12 +4,18 @@ public class n120826 {
     public static void main(String[] args) {
         n120826 sol = new n120826();
         String my_string = "BCBdbe";
-        char letter = 'B';
+        String letter = "B";
         System.out.println(sol.solution(my_string, letter));
     }
 
-    public String solution(String my_string, char letter) {
-        String answer = "";
-        return answer;
+    public String solution(String my_string, String letter) {
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < my_string.length(); i++) {
+            if (my_string.charAt(i) == letter.charAt(0)) continue;
+            answer.append(my_string.charAt(i));
+        }
+
+        return answer.toString();
     }
 }
