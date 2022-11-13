3a5c62e1716588b028d5da907083a8a678993450
kiarakim <kiarakim413@gmail.com>
2022-11-13 21:54:10 +0900

    feat(n120897) : add skeleton code

---
+++ src/programmers/n120897.java
@@ -0,0 +1,16 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class n120897 {
+    public static void main(String[] args) {
+        n120897 sol = new n120897();
+        int n = 24;
+        System.out.println(Arrays.toString(sol.solution(n)));
+    }
+
+    public int[] solution(int n) {
+        int[] answer = {};
+        return answer;
+    }
+}
