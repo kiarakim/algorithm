f973cabb1515f933a395ec5fa0ddd9fa99014c41
kiarakim <kiarakim413@gmail.com>
2022-11-23 13:09:43 +0900

    feat(n10818) : add n10818.java

---
+++ src/baekjoon/n10818.java
@@ -0,0 +1,25 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class n10818 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (min > num) min = num;
+            if (max < num) max = num;
+        }
+        System.out.printf("%d %d", min, max);
+    }
+}
