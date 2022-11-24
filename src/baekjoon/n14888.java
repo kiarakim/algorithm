70ea6a29101092db46c45aa96fcadb5d5c51ee0b
kiarakim <kiarakim413@gmail.com>
2022-11-24 18:33:46 +0900

    feat(n14888) : add n14888.java

---
+++ src/baekjoon/n14888.java
@@ -0,0 +1,63 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class n14888 {
+    public static int max = Integer.MIN_VALUE;
+    public static int min = Integer.MAX_VALUE;
+    public static int[] numbers;
+    public static int[] operator;
+    public static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        numbers = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        operator = new int[4];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 4; i++) {
+            operator[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dfs(numbers[0], 1);
+
+        System.out.println(max);
+        System.out.println(min);
+
+    }
+
+    public static void dfs(int num, int idx) {
+        if (idx == N) {
+            max = Math.max(max, num);
+            min = Math.min(min, num);
+            return;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            if (operator[i] > 0) {
+                operator[i]--;
+                switch (i) {
+                    case 0:
+                        dfs(num + numbers[idx], idx + 1); break;
+                    case 1:
+                        dfs(num - numbers[idx], idx + 1); break;
+                    case 2:
+                        dfs(num * numbers[idx], idx + 1); break;
+                    case 3:
+                        dfs(num / numbers[idx], idx + 1); break;
+                }
+                operator[i]++;
+            }
+        }
+    }
+}
