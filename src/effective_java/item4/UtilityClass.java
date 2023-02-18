7e576919cf770f8ba5f72a47b662caa7adb5ca93
kiarakim <kiarakim413@gmail.com>
2023-02-18 21:01:21 +0900

    feat(item4) : example from 백기선

--- src/effective_java/item4/UtilityClass.java
+++ src/effective_java/item4/UtilityClass.java
@@ -1,7 +1,12 @@
 package effective_java.item4;
 
+
 public class UtilityClass {
-	private UtilityClass(){
-		throw new AssertionError();
+	public static String getName(){
+		return "woowa";
+	}
+
+	public static void main(String[] args) {
+		String name = UtilityClass.getName();
 	}
 }
