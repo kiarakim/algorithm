98e99ce51ef341022003a24ef4fb48e49a2ba13c
kiarakim <kiarakim413@gmail.com>
2023-04-28 17:11:37 +0900

    feat: 모던 자바 인 액션 chap2 예제 코드

--- src/modern_java/chap02/FilterApples.java
+++ src/modern_java/chap02/FilterApples.java
@@ -1,6 +1,7 @@
 package modern_java.chap02;
 
 import java.util.Arrays;
+import static java.util.Comparator.comparing;
 import java.util.List;
 
 public class FilterApples {
@@ -10,13 +11,32 @@ public class FilterApples {
 			new Apple(155, "green"),
 			new Apple(120, "red")
 		);
-		prettyPrintApple(inventory, new AppleFancyFormatter());
-	}
+		//1단계 : 코드 전달
+		// inventory.sort(new AppleComparator());
 
-	public static void prettyPrintApple(List<Apple> inventory, AppleFormatter formatter){
-		for(Apple apple : inventory){
-			String output = formatter.accept(apple);
-			System.out.println(output);
+		//2단계 : 익명 클래스 사용
+		// inventory.sort(new Comparator<Apple>() {
+		// 	@Override
+		// 	public int compare(Apple a1, Apple a2) {
+		// 		return Integer.compare(a1.getWeight(), a2.getWeight());
+		// 	}
+		// });
+
+		//3단계 : 람파 표현식 사용
+		// inventory.sort(comparing(apple -> apple.getWeight()));
+
+		//4단계 : 메서드 참조 사용
+		inventory.sort(comparing(Apple::getWeight));
+
+		for (Apple apple : inventory) {
+			System.out.println(apple.getWeight());
 		}
 	}
+
+	// public static void prettyPrintApple(List<Apple> inventory, AppleFormatter formatter) {
+	// 	for (Apple apple : inventory) {
+	// 		String output = formatter.accept(apple);
+	// 		System.out.println(output);
+	// 	}
+	// }
 }
