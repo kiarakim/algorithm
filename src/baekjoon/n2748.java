d31b0f600af0e4c52831630639dc2e16b557e190
kiarakim <kiarakim413@gmail.com>
2023-07-16 00:01:13 +0900

    feat: 백준 피보나치 수 2 문제 풀이

--- src/baekjoon/n2748.java
+++ src/baekjoon/n2748.java
@@ -5,18 +5,28 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class n2748 {
+    static long[] arr;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int N = Integer.parseInt(br.readLine());
 
+        arr = new long[N + 1];
+
+        for (int i = 0; i < N + 1; i++) {
+            arr[i] = -1;
+        }
+
+        arr[0] = 0;
+        arr[1] = 1;
         System.out.println(fibonacci(N));
     }
 
-    private static int fibonacci(int n) {
-        if (n <= 1) {
-            return n;
+    private static long fibonacci(int N) {
+        if (arr[N] == -1) {
+            arr[N] = fibonacci(N - 1) + fibonacci(N - 2);
         }
-
-        return fibonacci(n - 1) + fibonacci(n - 2);
+        return arr[N];
     }
 }
