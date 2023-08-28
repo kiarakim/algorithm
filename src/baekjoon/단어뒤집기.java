deada3d3c1facdef8bb6f41bba64069b55fc62b5
kiarakim <kiarakim413@gmail.com>
2023-08-28 15:09:57 +0900

    refactor: 클래스이름 바꾸기

--- src/baekjoon/단어뒤집기.java
+++ src/baekjoon/단어뒤집기.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class no_9093 {
+public class 단어뒤집기 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
