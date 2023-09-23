c77b1ba528d1325063c14ebafdd5d8b22b87fec6
kiarakim <kiarakim413@gmail.com>
2023-09-23 23:03:21 +0900

    feat: 백준 SET2 파일합치기3

---
+++ src/main/java/algo/baekjoon/파일합치기3.java
@@ -0,0 +1,42 @@
+package algo.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class 파일합치기3 {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        Queue<Long> pq;
+
+        for (int t = 0; t < T; t++) {
+            int N = Integer.parseInt(br.readLine());
+            pq = new PriorityQueue<>();
+            st = new StringTokenizer(br.readLine());
+            for (int n = 0; n < N; n++) {
+                pq.add(Long.parseLong(st.nextToken()));
+            }
+
+            long save = 0;
+            while (pq.size() > 1) {
+                long chap1 = pq.poll();
+                long chap2 = pq.poll();
+
+                long sum = chap1 + chap2;
+
+                save += sum;
+                pq.add(sum);
+            }
+            sb.append(save).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
