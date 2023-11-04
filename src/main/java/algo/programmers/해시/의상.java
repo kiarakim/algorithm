2950b10608dbeecc2bdcf3e23b7c7f76a5221527
kiarakim <kiarakim413@gmail.com>
2023-11-04 13:02:33 +0900

    feat: 프로그래머스 의상

---
+++ src/main/java/algo/programmers/해시/의상.java
@@ -0,0 +1,61 @@
+package algo.programmers.해시;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class 의상 {
+    public static void main(String[] args) {
+        String[][] clothes = {
+                {"yellow_hat", "headgear"},
+                {"blue_sunglasses", "eyewear"},
+                {"green_turban", "headgear"}
+        };
+
+//        String[][] clothes = {
+//                {"a","A"},
+//                {"aa","A"},
+//                {"b","B"},
+//                {"bb","B"},
+//                {"bbb","B"},
+//                {"c","C"},
+//                {"cc","C"},
+//                {"ccc","C"},
+//                {"cccc","C"},
+//                {"d","D"},
+//                {"dd","D"},
+//                {"ddd","D"}
+//        };
+
+//        String[][] clothes = {{"crow_mask", "face"}, {"blue_sunglasses", "face"}, {"smoky_makeup", "face"}};
+        System.out.println(solution(clothes));
+    }
+
+    public static int solution(String[][] clothes) {
+        int answer = 1;
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String[] clothe : clothes) {
+            map.put(clothe[1], map.getOrDefault(clothe[1], 0) + 1);
+        }
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            answer *= entry.getValue() + 1;
+        }
+
+        return answer - 1;
+    }
+}
+/*
+A-a,aa
+B-b,bb,bbb
+C-c,cc,ccc
+D-d,dd,ddd,dddd
+
+1->12
+2->2*(3+3+4) + 3*(3+4) + 3*(4)
+3->(2*3)*(3+4) + (3*3) *4
+4->(2*3*3)*4
+ */
