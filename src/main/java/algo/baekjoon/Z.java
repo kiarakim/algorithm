b257575d07f20a889b8752c5d18e08ad377b094d
kiarakim <kiarakim413@gmail.com>
2023-11-17 15:42:37 +0900

    feat: 프로그래머스 Z

---
+++ src/main/java/algo/baekjoon/Z.java
@@ -0,0 +1,28 @@
+package algo.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Z {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        System.out.println(z(n, r, c));
+    }
+
+    private static int z(int n, int r, int c) {
+        if (n == 0) return 0;
+        int half = (int) Math.pow(2, n - 1);
+        if (r < half && c < half) return z(n - 1, r, c);
+        if (r < half && c >= half) return half * half + z(n - 1, r, c - half);
+        if (r >= half && c < half) return 2 * half * half + z(n - 1, r - half, c);
+        return 3 * half * half + z(n - 1, r - half, c - half);
+    }
+}
